4ebc67967811124737358be00a28168d22dad4a9
YongMing <516421926@qq.com>
2019-11-02 17:59:06 +0800

    到模块热替换

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import _ from 'lodash';
+import printMe from './print.js';
+
+function component() {
+
+    var element = document.createElement('div');
+    var btn = document.createElement('button');
+
+    element.innerText = _.join(['Hello','webpack'], ' ');
+
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+
+    element.appendChild(btn);
+
+    return element;
+}
+
+// document.body.appendChild(component());
+let element = component();
+document.body.appendChild(element);
+
+
+if(module.hot){
+    module.hot.accept('./print.js',function(){
+        console.log('111111Accepting the updated printMe module!');
+        // printMe();
+        document.body.removeChild(element);
+        element = component();
+        document.body.appendChild(element);
+    })
+}
